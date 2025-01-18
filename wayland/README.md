3d50054cbdaa0c7972dda60a766af11b4c84e29d
Vitaly Dyachkov <obyknovenius@me.com>
2025-01-18 14:11:45 +0100

    wayland: Draw a window with OpenGL ES

--- wayland/README.md
+++ wayland/README.md
@@ -1,3 +1,10 @@
 # References
 
-- [The Wayland Protocol](https://wayland-book.com/)
+## Wayland
+- [The Wayland Protocol](https://wayland-book.com)
+- [Programming Wayland Clients](https://jan.newmarch.name/Wayland)
+
+## EGL
+
+- [Khronos Native Platform Graphics Interface](https://registry.khronos.org/EGL/specs/eglspec.1.4.pdf)
+- [EXT_platform_base](https://registry.khronos.org/EGL/extensions/EXT/EGL_EXT_platform_base.txt)
--- wayland/client/client.c
+++ wayland/client/client.c
@@ -1,139 +1,31 @@
-#include <errno.h>
-#include <fcntl.h>
-#include <stdint.h>
-#include <stdio.h>
+
+#include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <time.h>
-#include <unistd.h>
 #include <wayland-client.h>
+#include <wayland-egl.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include <unistd.h>
 #include "xdg-shell-protocol.h"
 
-/* Shared memory support code*/
-static void
-randname(char *buf)
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    long r = ts.tv_nsec;
-    for (int i = 0; i < 6; ++i) {
-        buf[i] = 'A' + (r & 15) + (r & 16) * 2;
-        r >>= 5;
-    }
-}
-
-static int
-create_shm_file(void)
-{
-    int retries = 100;
-    do {
-        char name[] = "/wl_shm-XXXXXX";
-        randname(name + sizeof(name) - 7);
-        --retries;
-        int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
-        if (fd >= 0) {
-            shm_unlink(name);
-            return fd;
-        }
-    } while (retries > 0 && errno == EEXIST);
-    return -1;
-}
-
-int
-allocate_shm_file(size_t size)
-{
-    int fd = create_shm_file();
-    if (fd < 0) {
-        return -1;
-    }
-    int ret;
-    do {
-        ret = ftruncate(fd, size);
-    } while (ret < 0 && errno == EINTR);
-    if (ret < 0) {
-        close(fd);
-        return -1;
-    }
-    return fd;
-}
-
-/* Wayland code */
 struct client_state {
     /* Globals */
     struct wl_display *wl_display;
     struct wl_registry *wl_registry;
-    struct wl_shm *wl_shm;
     struct wl_compositor *compositor;
     struct xdg_wm_base *xdg_wm_base;
+
     /* Objects */
     struct wl_surface *wl_surface;
     struct xdg_surface *xdg_surface;
     struct xdg_toplevel *xdg_toplevel;
-};
 
-static void
-wl_buffer_release(void *data, struct wl_buffer *wl_buffer)
-{
-    /* Sent by the compositor when it's no longer using the buffer */
-    wl_buffer_destroy(wl_buffer);
-}
-
-static const struct wl_buffer_listener wl_buffer_listener = {
-    .release = wl_buffer_release,
-};
-
-static struct wl_buffer *
-draw_frame(struct client_state *state)
-{
-    const int width = 640, heigth = 480;
-    int stride = width * 4;
-    int size = stride * heigth;
-
-    int fd = allocate_shm_file(size);
-    if (fd == -1) {
-        return NULL;
-    }
-
-    uint32_t *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (data == MAP_FAILED) {
-        close(fd);
-        return NULL;
-    }
-
-    struct wl_shm_pool *pool = wl_shm_create_pool(state->wl_shm, fd, size);
-    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, 0, width, heigth, stride, WL_SHM_FORMAT_XRGB8888);
-    wl_shm_pool_destroy(pool);
-    close(fd);
-
-    /* Draw checkerboxed background */
-    for (int y = 0; y < heigth; ++y) {
-        for (int x = 0; x < width; ++x) {
-            if ((x + y / 8 * 8) % 16 < 8) {
-                data[y * width + x] = 0xFF666666;
-            } else {
-                data[y * width + x] = 0xFFEEEEEE;
-            }
-        }
-    }
-
-    munmap(data, size);
-    wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
-    return buffer;
-}
-
-static void
-xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial)
-{
-    struct client_state *state = data;
-    xdg_surface_ack_configure(xdg_surface, serial);
-
-    struct wl_buffer *buffer = draw_frame(state);
-    wl_surface_attach(state->wl_surface, buffer, 0, 0);
-    wl_surface_commit(state->wl_surface);
-}
-
-static const struct xdg_surface_listener xdg_surface_listener = {
-    .configure = xdg_surface_configure,
+    /* EGL */
+    struct wl_egl_window *egl_window;
+    EGLDisplay egl_display;
+    EGLConfig egl_conf;
+    EGLSurface egl_surface;
+    EGLContext egl_context;
 };
 
 static void
@@ -151,9 +43,7 @@ registry_global(void *data, struct wl_registry *wl_registry, uint32_t name,
                 const char *interface, uint32_t version)
 {
     struct client_state *state = data;
-    if (strcmp(interface, wl_shm_interface.name) == 0) {
-        state->wl_shm = wl_registry_bind(wl_registry, name, &wl_shm_interface, 1);
-    } else if (strcmp(interface, wl_compositor_interface.name) == 0) {
+    if (strcmp(interface, wl_compositor_interface.name) == 0) {
         state->compositor = wl_registry_bind(wl_registry, name, &wl_compositor_interface, 4);
     } else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
         state->xdg_wm_base = wl_registry_bind(wl_registry, name, &xdg_wm_base_interface, 1);
@@ -172,10 +62,75 @@ static const struct wl_registry_listener wl_registry_listener = {
     .global_remove = registry_global_remove,
 };
 
+static void
+init_egl(struct client_state *state)
+{
+    EGLint major, minor, count, n, size;
+    EGLConfig *configs;
+    int i;
+    EGLint config_attribs[] = {
+        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_ALPHA_SIZE, 8,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+        EGL_NONE
+    };
+
+    static const EGLint context_attribs[] = {
+        EGL_CONTEXT_CLIENT_VERSION, 2,
+        EGL_NONE
+    };
+
+    state->egl_display = eglGetDisplay((EGLNativeDisplayType)state->wl_display);
+
+    eglInitialize(state->egl_display, &major, &minor);
+
+    eglGetConfigs(state->egl_display, NULL, 0, &count);
+    configs = calloc(count, sizeof *configs);
+    eglChooseConfig(state->egl_display, config_attribs, configs, count, &n);
+
+    for (i = 0; i < n; ++i) {
+        eglGetConfigAttrib(state->egl_display, configs[i], EGL_BUFFER_SIZE, &size);
+        eglGetConfigAttrib(state->egl_display, configs[i], EGL_RED_SIZE, &size);
+        state->egl_conf = configs[i];
+        break;
+    }
+
+    state->egl_context = eglCreateContext(state->egl_display, state->egl_conf, EGL_NO_CONTEXT, context_attribs);
+}
+
+static void
+create_window(struct client_state *state)
+{
+    state->egl_window = wl_egl_window_create(state->wl_surface, 640, 480);
+    state->egl_surface = eglCreateWindowSurface(state->egl_display, state->egl_conf, state->egl_window, NULL);
+
+    eglMakeCurrent(state->egl_display, state->egl_surface, state->egl_surface, state->egl_context);
+}
+
+static void draw_blue(struct  client_state *state)
+{
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+    eglSwapBuffers(state->egl_display, state->egl_surface);
+}
+
+static void draw_red(struct  client_state *state)
+{
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+    eglSwapBuffers(state->egl_display, state->egl_surface);
+}
+
 int
 main(int argc, char *arg[])
 {
     struct client_state state = { 0 };
+
     state.wl_display = wl_display_connect(NULL);
     state.wl_registry = wl_display_get_registry(state.wl_display);
     wl_registry_add_listener(state.wl_registry, &wl_registry_listener, &state);
@@ -183,10 +138,16 @@ main(int argc, char *arg[])
 
     state.wl_surface = wl_compositor_create_surface(state.compositor);
     state.xdg_surface = xdg_wm_base_get_xdg_surface(state.xdg_wm_base, state.wl_surface);
-    xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener, &state);
     state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
     xdg_toplevel_set_title(state.xdg_toplevel, "Hello, Wayland!");
-    wl_surface_commit(state.wl_surface);
+
+    init_egl(&state);
+    create_window(&state);
+    draw_blue(&state);
+
+    sleep(2);
+
+    draw_red(&state);
 
     while (wl_display_dispatch(state.wl_display)) {
         /* This space deliberately left blank */
